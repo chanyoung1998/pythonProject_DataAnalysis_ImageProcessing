ccd0e183ce018233c8f6b0a9effa056f41351dcd
정찬영 <lms62111@naver.com>
2021-01-03 20:47:50 +0900

    NUMPY 파일 한 폴더에 정리

--- Numpy/example-2.py
+++ Numpy/example-2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #단일 객체 저장 및 불러오기
 array = np.arange(0,10)
-np.save('saved.npy',array)
+np.save('saved.npy', array)
 
 result = np.load('saved.npy')
 print(result)
@@ -10,7 +10,7 @@ print(result)
 #복수 객체 저장 및 불러오기
 array1 = np.arange(0,10)
 array2 = np.arange(10,20)
-np.savez('saved.npz',savedarray1 = array1,savedarray2 = array2)
+np.savez('saved.npz', savedarray1 = array1, savedarray2 = array2)
 
 data = np.load('saved.npz')
 result1 = data['savedarray1']
